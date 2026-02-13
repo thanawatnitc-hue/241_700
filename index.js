27a027700f7d09e93da6d4767b7a595690e4f667
thanawatnitc-hue <thanawat.nitc@ku.th>
2026-02-13 12:20:47 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
-function submitData() {
-  let firstNameDOM = document.querySelector('input[name=firstname]');
-  let lastNameDOM = document.querySelector('input[name=lastname]');
-  let ageDOM = document.querySelector('input[name=age]');
-  let genderDOM = document.querySelector('input[name=gender]:checked');
-  let interestDOMs = document.querySelectorAll('input[name=interest]:checked');
-  let descriptionDOM = document.querySelector('textarea[name=description]');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
 
-  let interest = ''
-  for (let i = 0; i < interestDOMs.length; i++) {
-    interest += interestDOMs[i].value
-    if (i != interestDOMs.length - 1) {
-      interest += ','
+app.use(bodyParser.json());
+
+let users = [];
+let counter = 1;
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter;
+    users.push(user);
+    counter += 1;
+    
+    res.json({
+        message: 'User added successfully',
+        user: user
+    });
+});
+
+app.put('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updateUser = req.body;
+    let selectedIndex = users.findIndex(user => user.id == id);
+
+    if (selectedIndex !== -1) {
+        if (updateUser.Fname) {
+            users[selectedIndex].Fname = updateUser.Fname;
+        }
+        if (updateUser.Lname) {
+            users[selectedIndex].Lname = updateUser.Lname;
+        }
+
+        res.json({
+            message: 'User updated successfully',
+            data: {
+                user: users[selectedIndex],
+                indexUpdate: selectedIndex
+            }
+        });
+    } else {
+        res.status(404).json({ message: 'User not found' });
     }
-  }
+});
 
-  let userData = {
-    firstName: firstNameDOM.value,
-    lastName: lastNameDOM.value,
-    age: ageDOM.value,
-    gender: genderDOM.value,
-    description: descriptionDOM.value,
-    interest: interest
-  }
-  console.log('submitData', userData);
-}
+app.delete('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let selectedIndex = users.findIndex(user => user.id == id);
+
+    if (selectedIndex !== -1) {
+        users.splice(selectedIndex, 1);
+        res.json({
+            message: 'User deleted successfully',
+            indexDelete: selectedIndex
+        });
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
